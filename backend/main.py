61977addb2d1f51e1498048c479f84bed54ca8aa
Xiaoye Mo <melico.moxy@gmail.com>
2021-10-23 18:29:59 -0400

    Update getfood

--- backend/main.py
+++ backend/main.py
@@ -12,23 +12,48 @@ def predict():
     history = []
     return
 
-@app.route("/getAllFood/<pagenum>")
+@app.route("/getAllFood/<pagenum>/")
 def getAllFood(pagenum):
-    # get data from database
-    # for each page, contains 20 items
-    foods = ["Milk, human", "Milk, NFS", "Milk, whole", "Milk, low sodium, whole"]
-    ids = [1,2,3,4]
-    calories = [70, 51, 60, 61]
+    '''
+    Example Return:
 
-    res = []
-    for id, food, calory in zip(ids, foods, calories):
-        res.append({
-            "food_id": id,
-            "food_name": food,
-            "energy": calory
-        })
+    {
+        "result": [
+            {
+                "food_id": 1,
+                "food_name": "banana",
+                "energy": 200,
+                "image": "https://upload.wikimedia.org/wikipedia/commons/8/8a/Banana-Single.jpg"
+            },
+            {
+                "food_id": 2,
+                "food_name": "apple",
+                "energy": 124,
+                "image": "https://static.wikia.nocookie.net/the-snack-encyclopedia/images/7/7d/Apple.png/revision/latest?cb=20200706145821"
+            }
+        ]
+    }
+    '''
+    try:
+        # get data from database
+        # for each page, contains 20 items
+        foods = ["Milk, human", "Milk, NFS", "Milk, whole", "Milk, low sodium, whole"]
+        ids = [1,2,3,4]
+        calories = [70, 51, 60, 61]
+        images = ["","","",""]
 
-    return {"result":res}
+        res = []
+        for id, food, calorie, image in zip(ids, foods, calories, images):
+            res.append({
+                "food_id": id,
+                "food_name": food,
+                "energy": calorie,
+                "image": image
+            })
+
+        return {"result": res, "status": 200}
+    except Exception as e:
+        return {"result": e, "status": 500}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
