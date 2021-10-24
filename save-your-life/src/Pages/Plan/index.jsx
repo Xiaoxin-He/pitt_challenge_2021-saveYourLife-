8ffba9933cc10bff8c4fa4d509469753d628a1a0
shirley he <HEX10@miamioh.edu>
2021-10-24 02:11:57 -0400

    fix dialog robot

--- save-your-life/src/Pages/Plan/index.jsx
+++ save-your-life/src/Pages/Plan/index.jsx
@@ -20,11 +20,11 @@ export default function Plan() {
     return (
         <ThemeProvider theme={theme}>
         <ChatBot
-        bubbleStyle={{ fontSize: '15px' }}
-        contentStyle={{ fontSize: '15px' }}
-        customStyle={{ fontSize: '15px' }}
+        bubbleStyle={{ fontSize: '17px' }}
+        contentStyle={{ fontSize: '17px' }}
+        customStyle={{ fontSize: '17px' }}
         // contentStyle={[{ "height": "100px"}]}
-        style= {{width: '100%'}}
+        style= {{width: '100%', height:"100%"}}
          steps={steps}
         />
         </ThemeProvider>
@@ -40,18 +40,48 @@ const steps = [
     {
      id:'intro-user', 
      options:[
-       {value:'y', label:'Good', trigger:'yes-response'},
-       {value:'n', label:'Bad', trigger:'no-response'},
+       {value:'y', label:'I am doing great! 😌', trigger:'yes-response'},
+       {value:'n', label:'I feel bad today 😟', trigger:'no-response'},
      ],
     },
     {
      id:'yes-response', 
      message:'Great!', 
-     end:true,
+     trigger:'intro2',
+    //  end:true,
     },
     {
      id:'no-response', 
-     message:'Sorry to hear that.', 
-     end:true,
+     message:'Sorry to hear that, could you tell me more about it? ', 
+     trigger:'intro2',
+    //  end:true,
     },
+    //next round
+    {
+        id:'intro2', 
+        message:'Did you have exercise today?', 
+        trigger:'intro-user2',
+    },
+    {
+        id:'intro-user2', 
+        options:[
+          {value:'y', label:'Yes, I did! 😆', trigger:'yes-response2'},
+          {value:'n', label:'Nope...I was too busy 😟', trigger:'no-response2'},
+        ],
+    },
+    {
+        id:'yes-response2', 
+        message:'Awesome! The records show you did exercising for 30 min', 
+        end:true,
+    },
+    {
+        id:'no-response2', 
+        message:'Excerising help you control your weight', 
+        // end:true,
+        trigger:'intro3',
+    },
+    {
+        id:'intro3', 
+        message:'Please follow this link to do a 10min exercise', 
+    }
    ]
